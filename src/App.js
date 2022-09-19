f25cd5c8542f7ea036c1d1763f87eb7d6907e20a
marahmanjs <imarahmandev@gmail.com>
2022-09-19 15:54:11 +0700

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
-import logo from './logo.svg';
-// import Timer from './components/timer';
-import Timer from './components/Timer.class'
-import Welcome from './components/Welcome';
-// import Event from './components/Event';
-import Toggle from './components/Toggle';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Timer instance="base" />
-        <Welcome name="A" />
-        {/* <Welcome name="B" />
-        <Welcome name="C" /> */}
-        {/* <Event /> */}
-        <Toggle />
-      </header>
+      
     </div>
   );
 }
